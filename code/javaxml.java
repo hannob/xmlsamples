ac3f8848ef9f42c4b359adf3d392b6a6c97d35a8
Hanno Böck <990588+hannob@users.noreply.github.com>
2025-11-08 11:31:30 +0100

    Add commented-out examples for secure parsing in java example

--- code/javaxml.java
+++ code/javaxml.java
@@ -11,6 +11,7 @@ import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.Transformer;
+import javax.xml.XMLConstants;
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
@@ -18,7 +19,24 @@ class javaxml {
     public static void main(String[] argv) {
 
         try {
-            DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            DocumentBuilderFactory dbfactory = DocumentBuilderFactory.newInstance();
+
+            // Different variations to secure against XXE, see also:
+            // https://cheatsheetseries.owasp.org/cheatsheets/XML_External_Entity_Prevention_Cheat_Sheet.html#java
+
+            // Disable DOCTYPE entirely
+            // dbfactory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+
+            // Disable Entities and DTD loading
+            // dbfactory.setFeature("http://xml.org/sax/features/external-general-entities", false);
+            // dbfactory.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+            // dbfactory.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+
+            // Poorly documented what this does exactly...
+            // https://docs.oracle.com/en/java/javase/11/docs/api/java.xml/javax/xml/XMLConstants.html#FEATURE_SECURE_PROCESSING
+            // dbfactory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+
+            DocumentBuilder builder = dbfactory.newDocumentBuilder();
             Document doc = builder.parse(new File(argv[0]));
 
             DOMSource dom = new DOMSource(doc);
